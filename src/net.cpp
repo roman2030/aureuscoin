d9f90a4d6e7b54c0997060be43ee8690d4c60c67
roman2030 <romanempire2030@gmail.com>
2018-01-18 21:27:37 -0600

    Updated connections

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"52.0.120.190", "52.0.120.190"},
+    //{"52.0.120.190", "52.0.120.190"},
+    {"34.228.253.152","34.228.253.152"},
     {"50.16.195.181", "50.16.195.181"},
     {"50.16.197.196", "50.16.197.196"}
 
